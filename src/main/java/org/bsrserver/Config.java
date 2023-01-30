090082ddd7c3f499d2fa1d87eb98e69b4a93bb1a
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2023-01-30 16:55:45 +0000

    feat: ✨ add config file

---
+++ src/main/java/org/bsrserver/Config.java
@@ -0,0 +1,56 @@
+package org.bsrserver;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+import com.moandjiezana.toml.Toml;
+
+public class Config {
+    private static final Config config = new Config();
+    private Toml configToml;
+
+    private Config() {
+    }
+
+    public static Config getInstance() {
+        return config;
+    }
+
+    public void loadConfig(Path dataDirectory) {
+        // check data directory
+        if (!dataDirectory.toFile().exists()) {
+            dataDirectory.toFile().mkdir();
+        }
+
+        // check file exists
+        File configFile = new File(dataDirectory.toAbsolutePath().toString(), "config.toml");
+        if (!configFile.exists()) {
+            try {
+                Files.copy(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("config.toml")), configFile.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        configToml = new Toml().read(configFile);
+    }
+
+    public String getDatabaseUrl() {
+        return configToml.getString("database.url");
+    }
+
+    public String getDatabaseUser() {
+        return configToml.getString("database.user");
+    }
+
+    public String getDatabasePassword() {
+        return configToml.getString("database.password");
+    }
+
+    public String getDatabaseTable() {
+        return configToml.getString("database.table");
+    }
+}

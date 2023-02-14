d93e178a7956164a24099c42cbf07f411f1696dd
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2023-02-14 16:21:00 +0000

    chore: 🔖 1.3.2

--- src/main/java/org/bsrserver/Main.java
+++ src/main/java/org/bsrserver/Main.java
@@ -20,7 +20,7 @@ import org.bsrserver.event.ServerConnectedEventEventListener;
 @Plugin(
         id = "bsrgreeter",
         name = "BSR Greeter",
-        version = "1.3.1",
+        version = "1.3.2",
         url = "https://www.bsrserver.org:8443",
         description = "A greeter",
         authors = {"Andy Zhang"}
